904cc3552811388d320f6b0d22c6aa049006e3e9
Rubén <ruben.rabaneda.bueno@mg.thedataincubator.com>
2018-02-06 23:31:08 +0000

    add app commit

---
+++ RRBapp/__init__.py
@@ -0,0 +1,17 @@
+from flask import Flask
+
+from .models import db
+from .views import tracking
+
+app = Flask(__name__)
+app.config.from_object('config')
+
+
+# Add the `constants` variable to all Jinja templates.
+@app.context_processor
+def provide_constants():
+    return {"constants": {"TUTORIAL_PART": 1}}
+
+db.init_app(app)
+
+app.register_blueprint(tracking)
